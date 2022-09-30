943e7ad36d499fd4c66276eb4e14adfb09637bd5
AXJN <83130553+AXJN@users.noreply.github.com>
2022-09-30 20:29:46 +0800

    LED5

--- 源/main.c
+++ 源/main.c
@@ -1556,7 +1556,7 @@ send_LED_RGB(255,0,0);
 #ifdef USE_RGB_LED
   LED_GPIO_init();
   GPIOB->BRR = LL_GPIO_PIN_8; // turn on red
-  GPIOB -> BRR =  LL_GPIO_PIN_5；
+  GPIOB->BRR = LL_GPIO_PIN_5；
   GPIOB->BSRR = LL_GPIO_PIN_3; //
 #endif
 
